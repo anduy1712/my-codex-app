682008ac2be10f76d840ba9df8d04d818ab05e29
An Phan <anphan@vulcanlabs.co>
2025-06-28 22:26:05 +0700

    feat(setup): kebab naming for files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,7 +11,27 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.config({
+    plugins: ["check-file"],
     extends: ["next/core-web-vitals", "next/typescript", "prettier"],
+    rules: {
+      "check-file/filename-naming-convention": [
+        "error",
+        {
+          "**/*.{ts,tsx}": "KEBAB_CASE",
+        },
+        {
+          // ignore the middle extensions of the filename to support filename like bable.config.js or smoke.spec.ts
+          ignoreMiddleExtensions: true,
+        },
+      ],
+      "check-file/folder-naming-convention": [
+        "error",
+        {
+          // all folders within src (except __tests__)should be named in kebab-case
+          "src/**/!(__tests__)": "KEBAB_CASE",
+        },
+      ],
+    },
   }),
 ];
 
